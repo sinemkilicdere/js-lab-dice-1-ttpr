2072d1dc6cf974885f5c5a1db4cd3e96a2a7a136
Sinem Kilicdere <sinemkilicdere@gmail.com>
2025-05-01 12:02:17 -0400

    Completed Dice Roller Assignment

--- js/main.js
+++ js/main.js
@@ -3,16 +3,33 @@ let dice = {
   roll: function () {
   let randomNumber;
   // Write Code Here
+  randomNumber = Math.floor(Math.random() * this.sides) + 1;
+  
   // Generate a random number between 1 and the number of sides and assign it to randomNumber
 
    return (randomNumber);
   }
 };
 
-function printNumber(number) {
-  let placeholder = document.getElementById("placeholder");
-  placeholder.innerHTML = number;
+
+  function printNumber(number) {
+  const numberToWord = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six'
+  };
+
+  let word = numberToWord[number];
+  let diceImage = document.getElementById("dice-image");
+  diceImage.src = `img/dice-six-faces-${word}.png`;
+  diceImage.alt = `Dice shows ${number}`;
 }
+
+  
+
 
 const button = document.getElementById("button");
 
